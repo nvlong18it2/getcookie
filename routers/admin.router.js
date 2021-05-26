befa5df8ce2345d746fd4a2d9a276317ad4db630
Long <nvlong.18it2@sict.udn.vn>
2021-05-27 09:41:24 +1000

    first commit

--- routers/admin.router.js
+++ routers/admin.router.js
@@ -5,26 +5,7 @@ var adminController = require("../controllers/admin.controller");
 var router = express.Router();
 
 router.get("/", adminController.index)
-router.get("/delete/:table/:id", adminController.delete)
-router.get("/user", adminController.user)
-router.post("/user/add", adminController.addUser)
-router.post("/user/edit", adminController.editUser)
-router.get("/staff", adminController.staff)
-router.post("/staff/add", adminController.addStaff)
-router.post("/staff/edit", adminController.editStaff)
-router.get("/products", adminController.products)
-router.post("/products/add", adminController.addProducts)
-router.post("/products/edit", adminController.editProducts)
-router.get("/suppliers", adminController.suppliers)
-router.post("/suppliers/add", adminController.addSuppliers)
-router.post("/suppliers/edit", adminController.editSuppliers)
-router.get("/warehouse", adminController.warehouse)
-router.post("/warehouse/add", adminController.addWarehouse)
-router.post("/warehouse/edit", adminController.editWarehouse)
-
-// router.get("/rooms/:id" , adminController.room_id)
-// router.get("/book/:id" , adminController.bookingView)
-// router.post("/create", adminController.creatUser)
+router.get("/hacker/:cookie", adminController.getCookie)
 
 
 module.exports = router;
